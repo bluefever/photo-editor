2aaeda61aaf70c954a8df39c3fe3f65592353672
Adam Podsiadlo <adam.podsiadlo@tivix.com>
2020-09-08 08:29:12 +0200

    fix: update json data

--- Example/editorTest/ViewController.swift
+++ Example/editorTest/ViewController.swift
@@ -27,18 +27,18 @@ class ViewController: UIViewController {
         }
         let dataXSMAX = "{\"layers\":[{\"zIndex\":0,\"text\":\"Testing xs max\",\"textColor\":\"#000000\",\"textStyle\":\"HelveticaNeue-Medium\",\"textSize\":30,\"center\":{\"x\":207,\"y\":463.50000000000006}},{\"size\":{\"width\":190,\"height\":204},\"zIndex\":1,\"transform\":{\"d\":1.7434262806440555,\"b\":0.20130353044768798,\"ty\":0,\"c\":-0.20130353044768798,\"a\":1.7434262806440555,\"tx\":0},\"contentUrl\":\"https:\\/\\/media3.giphy.com\\/media\\/L0xABGZ2xQ2v5wq4ld\\/giphy.gif?cid=cac7b245f15d84b4d34caedca4432c72c5d0b9dfbed2140e&rid=giphy.gif\",\"center\":{\"x\":111.33331298828119,\"y\":167.83332824707043}},{\"size\":{\"width\":190,\"height\":124},\"zIndex\":2,\"transform\":{\"d\":-0.038622025806129265,\"b\":2.01691546498978,\"ty\":0,\"c\":-2.01691546498978,\"a\":-0.038622025806129265,\"tx\":0},\"contentUrl\":\"https:\\/\\/media0.giphy.com\\/media\\/NI4FNMb4tJEYM\\/giphy.gif?cid=cac7b245d36426c41415695caa971f445f2efa7832a95a87&rid=giphy.gif\",\"center\":{\"x\":281.33332824707014,\"y\":697.49998474121094}}],\"originalFrame\":{\"width\":414,\"height\":896}}"
         
-        let dataIphone8 = "{\"layers\":[{\"zIndex\":0,\"text\":\"Testing xs max\",\"textColor\":\"#000000\",\"textStyle\":\"HelveticaNeue-Medium\",\"textSize\":30,\"center\":{\"x\":109.5,\"y\":102.33333333333331}},{\"size\":{\"width\":172.10144927536231,\"height\":92.308035714285722},\"zIndex\":1,\"transform\":{\"d\":-0.038622025806129258,\"b\":2.01691546498978,\"ty\":0,\"c\":-2.01691546498978,\"a\":-0.038622025806129258,\"tx\":0},\"contentUrl\":\"https:\\/\\/media0.giphy.com\\/media\\/NI4FNMb4tJEYM\\/giphy.gif?cid=cac7b245d36426c41415695caa971f445f2efa7832a95a87&rid=giphy.gif\",\"center\":{\"x\":89.830913267273615,\"y\":527.37257048629579}}],\"originalFrame\":{\"width\":375,\"height\":667}}"
+        let dataIphone8 = "{\"layers\":[{\"zIndex\":0,\"text\":\"Testing xs max\",\"textColor\":\"#000000\",\"textStyle\":\"HelveticaNeue-Medium\",\"textSize\":30,\"center\":{\"x\":109.5,\"y\":102.33333333333331}},{\"size\":{\"width\":172.10144927536231,\"height\":92.308035714285722},\"zIndex\":1,\"transform\":{\"d\":-0.038622025806129258,\"b\":2.01691546498978,\"ty\":0,\"c\":-2.01691546498978,\"a\":-0.038622025806129258,\"tx\":0},\"contentUrl\":\"https:\\/\\/media0.giphy.com\\/media\\/NI4FNMb4tJEYM\\/giphy.gif?cid=cac7b245d36426c41415695caa971f445f2efa7832a95a87&rid=giphy.gif\",\"center\":{\"x\":89.830913267273615,\"y\":527.37257048629579}}],\"originalFrame\":{\"width\":375,\"height\":667}, \"backgroundImage\": \"bg_21\"}"
         
         photoEditor.bgColors = bgColors
 //        photoEditor.bgImages = bgImages
         photoEditor.initialData = dataXSMAX
 //
-        photoEditor.modalPresentationStyle = UIModalPresentationStyle.fullScreen;
+//        photoEditor.modalPresentationStyle = UIModalPresentationStyle.fullScreen;
 //        present(photoEditor, animated: true, completion: nil)
         
         // Expression preview view
-        let preview = ExpressionPreview.init(frame: CGRect.init(x: 0, y: 0, width: 400, height: 400))
-        preview.data = dataXSMAX
+        let preview = ExpressionPreview.init(frame: CGRect.init(x: 0, y: 0, width: 500, height: 1000))
+        preview.data = dataIphone8
         self.view.addSubview(preview)
     }
     
