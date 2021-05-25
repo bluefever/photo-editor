26dacb0d85dec000b6f6f0496cc2e6507ac2f89f
Adam Podsiadlo <adam.podsiadlo@tivix.com>
2021-05-25 10:45:01 +0200

    fix: remove fonts from copy resource

--- Photo Editor/Photo Editor/PhotoEditor+Font.swift
+++ Photo Editor/Photo Editor/PhotoEditor+Font.swift
@@ -11,7 +11,7 @@ import UIKit
 
 extension PhotoEditorViewController {
     static var fonts = ["Nunito-ExtraBold", "Nunito-SemiBold", "Nunito-Black",
-                        "ShadowsIntoLight", "Cheria", "SundayMorningRegular", "FastInMyCar", "ZillaSlabHighlight-Bold", "BowlbyOneSC-Regular", "DMSans-Bold", "Poppins-Bold"]
+                        "ShadowsIntoLight", "Cheria", "SundayMorningRegular", "FastInMyCar", "ZillaSlabHighlight-Bold", "BowlbyOneSC-Regular"]
     
     //Resources don't load in main bundle we have to register the font
     func registerFont() {
